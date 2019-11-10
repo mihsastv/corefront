869b2168b8a4137a4c8ab292985d984712dbe761
mihsastv <30728392+mihsastv@users.noreply.github.com>
2019-11-10 19:02:18 +0300

    2

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import 'materialise-css/dist/js/materialize.js'
+
 
 if (environment.production) {
   enableProdMode();
